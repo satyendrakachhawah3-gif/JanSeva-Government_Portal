970eea80a7dcdcbfdbbed0278e25a5a11cb6b57a
satyendrakachhawah3-gif <satyendrakachhawah3@gmail.com>
2026-08-31 22:13:14 +0530

    add frontend state and sector dropdown constants

---
+++ client/src/utils/constants.js
@@ -0,0 +1,42 @@
+/**
+ * Frontend Dropdown Options & Metadata Dictionary
+ */
+
+export const INDIAN_STATES = [
+  'All India',
+  'Maharashtra',
+  'Delhi',
+  'Karnataka',
+  'Gujarat',
+  'Uttar Pradesh',
+  'Tamil Nadu',
+  'Rajasthan',
+  'West Bengal',
+  'Madhya Pradesh',
+  'Punjab',
+  'Kerala'
+];
+
+export const SCHEME_CATEGORIES = [
+  'Education',
+  'Agriculture',
+  'Employment',
+  'Women & Child Welfare',
+  'Health',
+  'Housing',
+  'Financial Assistance',
+  'Entrepreneurship',
+  'Senior Citizens',
+  'Disability Support',
+  'Social Welfare'
+];
+
+export const OCCUPATIONS = [
+  'Student',
+  'Farmer',
+  'Unemployed',
+  'Self Employed',
+  'Private Service',
+  'Government Service',
+  'Other'
+];
